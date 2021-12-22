8cee9e30e5ca2e27984c5c09e07eaf540a442bf6
Joel Vargas <joelvargasapolinario@gmail.com>
2021-12-22 18:21:42 -0500

    sidebar style

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className={"sidebar"}></div>
-      <div className="header-container">
-        <div className={"header-name"}></div>
-        <div className={"header-constrast"}></div>
+      <div className={"sidebar"}>
+        <div className="header-container">
+          <div className={"header-name"}></div>
+          <div className={"header-constrast"}></div>
+        </div>
+
+        <div className={"sidebar-font-heading"}>{"EDUCATION"}</div>
       </div>
     </>
   );
