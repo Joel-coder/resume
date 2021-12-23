2643aed9790e1b623ea60474a26775c2bf6f68fe
Joel Vargas <joelvargasapolinario@gmail.com>
2021-12-22 23:50:30 -0500

    Transition components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import logo from "./logo.svg";
-import "./App.css";
-
+import "./App.scss";
+import Card from "react-bootstrap/Card";
+import "typeface-poppins";
 function App() {
   return (
     <>
       <div className={"sidebar"}>
         <div className="header-container">
-          <div className={"header-name"}></div>
+          <div className={"header-name"}>{"Joel Vargas"}</div>
           <div className={"header-constrast"}></div>
         </div>
-
-        <div className={"sidebar-font-heading"}>{"EDUCATION"}</div>
+        <div className="ms-4">
+          <div className={"sidebar-font-heading"}>{"ABOUT"}</div>
+          <div className={"sidebar-font-heading"}>{"EXPERIENCES"}</div>
+          <div className={"sidebar-font-heading"}>{"EDUCATION"}</div>
+          <div className={"sidebar-font-heading"}>{"SKILLS"}</div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="me-4"></div>
       </div>
     </>
   );
