d6dc6376f225f3e0b314d532ed95ae02812b0a41
Joel Vargas <joelvargasapolinario@gmail.com>
2021-12-30 15:59:34 -0500

    adding profile picture

--- pages/landingPage/index.js
+++ pages/landingPage/index.js
@@ -32,31 +32,51 @@ deepPick("data.info.fullname.first", dan); // "Dan"
 export default function LandingPage() {
   return (
     <>
-      <Navbar bg="light" expand="md" className={"nav-bar py-3"}>
-        <Container fluid>
-          <Navbar.Brand href="#home" className={"ms-2"}>
-            Joel Vargas
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <div className="w-100 h-100">
+        <Navbar bg="light" expand="md" className={"nav-bar py-3"}>
+          <Container fluid>
+            <Navbar.Brand href="#home" className={"ms-2"}>
+              Joel Vargas
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="d-flex justify-content-end w-100">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#home">Resume</Nav.Link>
-              <Nav.Link href="#home">Projects</Nav.Link>
-              <Nav.Link href="#home">Contact</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="d-flex justify-content-end w-100">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#home">Resume</Nav.Link>
+                <Nav.Link href="#home">Projects</Nav.Link>
+                <Nav.Link href="#home">Contact</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
 
-      <div className={"bg-left"}></div>
+        <div className={"bg-left"}></div>
 
-      <div className={"bg-right"}></div>
-      <div className="d-flex flex-row justify-content-center align-items-center h-100 ">
-        <div className="middle-container d-flex flex-row">
-          <div className={"bg-right-inner"}></div>
-          <div className={"bg-left-inner"}></div>
+        <div className={"bg-right"}></div>
+        <div
+          className={
+            "d-flex flex-row justify-content-center align-items-center w-100 h-100"
+          }
+        >
+          <div className="middle-container ">
+            <div className={"bg-right-inner"}>
+              <div className={"p-3"}>
+                <img
+                  src="https://i.guim.co.uk/img/media/d31ebd49b32a5aa609a584ababb1e03bc70b4942/573_213_2929_1758/master/2929.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=31c49726c022c1d41ee9b9b6b7254b46"
+                  alt="Avatar"
+                  class="avatar"
+                />
+              </div>
+            </div>
+            <div className={"bg-left-inner"}>
+              <img
+                src="https://i.guim.co.uk/img/media/d31ebd49b32a5aa609a584ababb1e03bc70b4942/573_213_2929_1758/master/2929.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=31c49726c022c1d41ee9b9b6b7254b46"
+                alt="Avatar"
+                class="avatar"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>
