1087a892b63244c10bbc4d4f997a280905db09e1
Joel Vargas <joelvargasapolinario@gmail.com>
2021-12-30 14:24:29 -0500

    style change

--- pages/landingPage/index.js
+++ pages/landingPage/index.js
@@ -2,50 +2,59 @@ import React, { useEffect, useState } from "react";
 import { useTranslation } from "next-i18next"; // i18n
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"; // i18n
 import { useRouter } from "next/router"; // i18n
-import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+
 import SideBar from "../../components/Sidebar";
+const deepPick = (fields, object = {}) => {
+  const [first, ...remaining] = fields.split(".");
+  return remaining.length
+    ? (deepPick(remaining.join("."), object[first]), console.log(first))
+    : object[first];
+};
+const dan = {
+  type: "person",
+  data: {
+    gender: "male",
+    info: {
+      id: 22,
+      fullname: {
+        first: "Dan",
+        last: "Deacon",
+      },
+    },
+  },
+};
+
+deepPick("data.info.fullname.first", dan); // "Dan"
+
 export default function LandingPage() {
   return (
     <>
-      <Col className="container-all" xs={12}>
-        <SideBar />
-        <Col className="card-container">
-          <Card style={{ width: "50rem", maxHeight: "50px" }}>
-            <Card.Img
-              variant="top"
-              src="https://lh3.googleusercontent.com/proxy/1QMoc28Djm9qai28SMIe0CHX20ifTvtl1Ci-pnVeoZnpXCJrQ_lj731s8rFhh1a0ttxQfk_JKPUCLYwDrnVEt9EsQ5CRWOYMLNkk_WTNWsxHkBA"
-            />
-            <Card.Body className="d-flex flex-row card-bg">
-              <Col>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Col>
-              <Col>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Col>
-              <Col>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Col>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Col>
+      <Navbar bg="light" expand="md" className={"nav-bar py-3"}>
+        <Container fluid>
+          <Navbar.Brand href="#home" className={"ms-2"}>
+            Joel Vargas
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="d-flex justify-content-end w-100">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#home">Resume</Nav.Link>
+              <Nav.Link href="#home">Projects</Nav.Link>
+              <Nav.Link href="#home">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className={"bg-left"}></div>
+
+      <div className={"bg-right"}></div>
+
+      <Container fluid>{"klkddd"}</Container>
     </>
   );
 }
