a852f93322df337b3f75abe9eda5fbcf88b48210
Joel Vargas <joelvargasapolinario@gmail.com>
2021-12-31 18:51:34 -0500

    adding resume section

---
+++ pages/resume/index.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+
+export default function Resume() {
+  return (
+    <>
+      <div
+        className={
+          "d-flex flex-column justify-content-center align-items-center"
+        }
+      >
+        <div className="box"></div>
+      </div>
+    </>
+  );
+}
