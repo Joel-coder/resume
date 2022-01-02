59508dd1e37f425d83a0c54f30a664afa75d8eb7
Joel Vargas <joelvargasapolinario@gmail.com>
2022-01-01 19:16:58 -0500

    Intersection observer

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from "react";
-
+import colors from "./css-colors";
+import FadeInSection from "../../components/FadeInSection";
+import NavBar from "../../components/NavBar";
 export default function Resume() {
   return (
     <>
-      <div
-        className={
-          "d-flex flex-column justify-content-center align-items-center"
-        }
-      >
-        <div className="box"></div>
+      <NavBar />
+      <div className="main-container">
+        <h1>All the CSS colors!</h1>
+
+        {colors.map((color) => (
+          <FadeInSection key={color}>
+            <div className="box" style={{ backgroundColor: color }}>
+              <span>{color}</span>
+            </div>
+          </FadeInSection>
+        ))}
       </div>
     </>
   );
