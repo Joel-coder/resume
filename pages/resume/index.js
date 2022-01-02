62243f7790e33a79dfe24429e35c2342d1857e5c
Joel Vargas <joelvargasapolinario@gmail.com>
2022-01-01 20:09:14 -0500

    testing array of objects

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -3,15 +3,38 @@ import colors from "./css-colors";
 import FadeInSection from "../../components/FadeInSection";
 
 export default function Resume() {
+  const [profileInfo, setProfileInfo] = useState([
+    {
+      company: "Centennial College",
+      position: "Front-end Developer",
+      startDate: "May, 2021",
+      endDate: "Dec, 2021",
+      country: "Canada",
+      description: "Description",
+      technolgies: "Technologies",
+      color: colors[0],
+    },
+    {
+      company: "Centennial College",
+      position: "Front-end Developer",
+      startDate: "May, 2021",
+      endDate: "Dec, 2021",
+      country: "Canada",
+      description: "Description",
+      technolgies: "Technologies",
+      color: colors[1],
+    },
+  ]);
+
   return (
     <>
       <div className="main-container">
         <h1>All the CSS colors!</h1>
 
-        {colors.map((color) => (
-          <FadeInSection key={color}>
-            <div className="box" style={{ backgroundColor: color }}>
-              <span>{color}</span>
+        {profileInfo.map((profileInfo, index) => (
+          <FadeInSection key={index}>
+            <div className="box" style={{ backgroundColor: profileInfo.color }}>
+              <span>{profileInfo.company}</span>
             </div>
           </FadeInSection>
         ))}
