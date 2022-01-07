f4a19634de308fa4ef1483729bafdd55dc4be71f
Joel Vargas <joelvargasapolinario@gmail.com>
2022-01-07 14:39:15 -0500

    remove img

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -23,7 +23,6 @@ export default function Project() {
         <div className=" d-flex flex-row justify-content-around w-75">
           {projectsInfo.map((info, index) => (
             <Card style={{ width: "18rem" }} className="card">
-              <Card.Img variant="top" src="holder.js/100px180" />
               <Card.Body>
                 <Card.Text>{info.Description}</Card.Text>
                 <Card.Text>{info.Technologies}</Card.Text>
