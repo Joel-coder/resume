d528396d6e7a2580f1f96808d32e9fc40c0efabb
Joel Vargas <joelvargasapolinario@gmail.com>
2022-01-19 19:58:26 -0500

    refactoring NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -7,18 +7,12 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 export default function NavBar() {
   const router = useRouter();
-  const countRef = useRef("");
-  var jsonobj =
-    '{ "name":"Brendan Eich","designerof":"Javascript","bornin":"1961" }';
-
-  // Here we convert JSON to object
 
   return (
     <Navbar bg="light" expand="md" className={"nav-bar py-3"}>
       <Container fluid>
         <Navbar.Brand className={"ms-2"}>Joel Vargas</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="d-flex justify-content-end w-100">
             <Nav.Link onClick={() => router.push("/")}>Home</Nav.Link>
