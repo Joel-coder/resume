920d79dd65e03137fbc042fa0a4a7ef1e1196382
Joel Vargas <joelvargasapolinario@gmail.com>
2022-10-18 15:24:08 -0400

    container

--- components/MiddleContainer.js
+++ components/MiddleContainer.js
@@ -17,11 +17,7 @@ export default function MiddleContainer() {
         <p className="subheading-left-inner">
           {"What do I do & what I am looking for?"}
         </p>
-        <p className="text-left-inner">
-          {
-            "I am an engineer with almost 5 years of experience in the IT industry. In the last 2 years I started focusing more on developing software solutions, and I spent the last year working exclusively with software development. I strive to develop the highest quality by following SOLID principles. I value long term results and take ownership of the work that needs to be done. Due to my curious nature, I excel in places where I am constantly learning and improving."
-          }
-        </p>
+        <p className="text-left-inner">{}</p>
       </div>
       <div className={"bg-right-inner h-100"}>
         <div className={"p-3"}>
